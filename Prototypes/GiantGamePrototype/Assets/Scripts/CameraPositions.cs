c4f5ef99bb2a13552917f01e6579163e1fcd05bb
Elliot Chester <elliotchester@outlook.com>
2018-10-12 13:52:50 +0100

    Added the giant game prototype
    Uploaded the giant game prototype

---
+++ Prototypes/GiantGamePrototype/Assets/Scripts/CameraPositions.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPositions : MonoBehaviour {
+
+    public float speed;
+
+    public Vector3 CameraOut;
+    public float OrthoOut;
+
+    public Vector3 CameraInLeft;
+    public float OrthoInLeft;
+
+    public Vector3 CameraInRight;
+    public float OrthoInRight;
+
+    public bool useDynamic;
+    public Vector3 Dynamic;
+    public float DynamicOrtho;
+
+    Vector3 targetPos;
+    float targetSize;
+
+    // Use this for initialization
+    void Start () {
+        targetPos = CameraOut;
+        targetSize = OrthoOut;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            targetPos = CameraOut;
+            targetSize = OrthoOut;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            targetPos = CameraInLeft;
+            targetSize = OrthoInLeft;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            targetPos = CameraInRight;
+            targetSize = OrthoInRight;
+        }
+
+        if (useDynamic)
+        {
+            this.transform.position = Vector3.Lerp(transform.position, Dynamic, Time.deltaTime * speed);
+            this.GetComponent<Camera>().orthographicSize = Mathf.Lerp(this.GetComponent<Camera>().orthographicSize, DynamicOrtho, Time.deltaTime * speed);
+        }
+        else
+        {
+            this.transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * speed);
+            this.GetComponent<Camera>().orthographicSize = Mathf.Lerp(this.GetComponent<Camera>().orthographicSize, targetSize, Time.deltaTime * speed);
+        }
+
+    }
+}
---
+++ Prototypes/GiantGamePrototype/Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableObject : MonoBehaviour {
+
+    GameObject giant;
+
+	// Use this for initialization
+	void Start ()
+    {
+        giant = GameObject.FindGameObjectWithTag("Giant");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("click");
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider == this.GetComponent<BoxCollider2D>() || hit.collider == this.GetComponent<CircleCollider2D>() || hit.collider == this.GetComponent<PolygonCollider2D>())
+            {
+                giant.GetComponent<WalkToPoint>().MoveToPoint(hit.collider.gameObject);
+            }
+        }
+    }
+}
