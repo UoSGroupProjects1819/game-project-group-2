4de465dec553c5a1d4f27f03e7dda7b69236a8b3
Elliot Chester <elliotchester@outlook.com>
2019-02-28 11:39:29 +0000

    updated powerpoint

---
+++ Prototypes/GiantGamePrototype/Assets/Scripts/ChestScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestScript : MonoBehaviour
+{
+    public GameObject[] chests;
+
+    void ClickChest(GameObject chest)
+    {
+        foreach (var item in chests)
+        {
+            if(item != chest)
+            {
+                item.SetActive(false);
+            }
+        }
+
+        chest.GetComponent<Animator>().SetTrigger("Open");
+    }
+}
